b9860170e33639c19c6ca69062d181ad6a8ae6f8
Rajesh Pradhan <pradhan143rajesh@gmail.com>
2023-05-12 15:07:04 +0530

    sadasd

--- Backend/routes/food.route.js
+++ Backend/routes/food.route.js
@@ -3,13 +3,15 @@ const foodRouter = express.Router();
 const { FoodModel } = require("../models/food.model");
 
 // CREATE FOOD
-foodRouter.post('/add',async(req,res)=>{
-    try {
-        const food = FoodModel(req.body);
-        await food.save();
-    } catch (error) {
-        
-    }
-})
+foodRouter.post("/add", async (req, res) => {
+  try {
+    const food = FoodModel(req.body);
+    await food.save();
+  } catch (error) {}
+});
 
-module.exports = { userRouter };
+foodRouter.get("/", (req, res) => {
+  res.send("WElcome to Food Page");
+});
+
+module.exports = { foodRouter };
