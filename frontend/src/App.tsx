48b41ebd16277cc5ab66a42ad8401af914df748f
ramsurat0880 <surajkumar231501@gmail.com>
2023-05-13 10:10:56 +0530

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import "./App.css";
 import Allroutes from "./routes/Allroutes";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      <Navbar />
-      <Allroutes />
-    </h1>
+    <div>
+      <Navbar/>
+      <Allroutes/>
+    </div>
   );
 }
 export default App;
