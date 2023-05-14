9e45ee3787d38977190b0dcb1f967bb65437f414
ramsurat0880 <surajkumar231501@gmail.com>
2023-05-14 07:37:47 +0530

    frontend

--- frontend/src/pages/Exercise.tsx
+++ frontend/src/pages/Exercise.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import logo from '../logo/NutriFit_Ninjas-removebg-preview.png';
 import ExerciseCard from './ExerciseCard';
 import axios, { AxiosResponse } from 'axios';
+import Footer from '../components/Footer';
 
 interface Data {
   id: number;
@@ -28,6 +29,7 @@ export default function Exercise() {
   }, []);
 
   return (
+    <>
     <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-2">
       <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
       <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
@@ -48,6 +50,9 @@ export default function Exercise() {
         ))}
         
       </div>
+  
     </section>
+    <Footer/>
+    </>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import image from "./yoga.jpg"
+import Footer from '../components/Footer'
  
 
 export default function Home() {
   
 
   return (
+    <>
       <img src={image} className="w-full h-auto" alt="Image" />
+      <Footer/>
+      </>
   ) 
 }
 
